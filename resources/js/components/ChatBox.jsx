90cb9ff12f752439d7feaeeae386d2e6ea2f4af9
Artem <artemanciferov26365@gmail.com>
2025-09-11 22:35:12 +0300

    reverb upd 7

--- resources/js/components/ChatBox.jsx
+++ resources/js/components/ChatBox.jsx
@@ -13,7 +13,7 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 6001,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 6001,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['wss', 'ws'],
     
